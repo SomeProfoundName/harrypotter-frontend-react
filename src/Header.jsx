d70882eb8aff0f4a242717cc5789102ae968c874
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-06-21 15:30:30 +0100

    First initalise. Add Header to Page

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header() {
+    return(
+        <header>
+            <div className="title">
+                <h1>Harry Potter World</h1>
+            </div>
+            <div className="search">
+                <input type="text" id="search-bar" placeholder="  &#x1F50E;&#xFE0E; Search "/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
